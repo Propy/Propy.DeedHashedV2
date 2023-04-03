68380213e748eaadb6934c812c73d63ee57437ab
Jay Welsh <thealchemicalopus@gmail.com>
2023-04-03 13:06:21 +0200

    feat(DeedHashedV2): role-based auth (Ownable -> AccessControl)

--- contracts/DeedHashedV2.sol
+++ contracts/DeedHashedV2.sol
@@ -3,11 +3,11 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol"; 
 
 import "./DeedHashedTypes.sol";
 
-contract DeedHashedV2 is ERC721, Ownable {
+contract DeedHashedV2 is ERC721, AccessControl {
 
   using Counters for Counters.Counter;
   Counters.Counter private _tokenIdCounter;
@@ -16,21 +16,59 @@ contract DeedHashedV2 is ERC721, Ownable {
   event TokenStatusUpdated(uint256 indexed tokenId, DeedHashedTypes.TokenStatus indexed tokenStatus, string indexed tokenURI);
   event TokenURIUpdated(uint256 indexed tokenId, DeedHashedTypes.TokenStatus indexed tokenStatus, string indexed tokenURI);
 
+  bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE"); // can mint
+  bytes32 public constant STATUS_UPDATER_ROLE = keccak256("STATUS_UPDATER_ROLE"); // can manage status of tokens
+  bytes32 public constant TOKEN_URI_UPDATER_ROLE = keccak256("TOKEN_URI_UPDATER_ROLE"); // can update tokenURI
 
   mapping (uint256 => DeedHashedTypes.Token) internal tokens;
 
   constructor(
-    address _admin,
+    address _roleAdmin,
     string memory _tokenName,
     string memory _tokenSymbol
   ) ERC721(_tokenName, _tokenSymbol) {
-    _transferOwnership(_admin);
+    _setupRole(DEFAULT_ADMIN_ROLE, _roleAdmin);
+    _setupRole(MINTER_ROLE, _roleAdmin);
+    _setupRole(STATUS_UPDATER_ROLE, _roleAdmin);
+    _setupRole(TOKEN_URI_UPDATER_ROLE, _roleAdmin);
   }
+
+  // TODO: Test gas consumption comparison between current method and using `using Roles for Roles.Role;`
+
+  modifier onlyRoleAdmin() {
+    require(hasRole(DEFAULT_ADMIN_ROLE, msg.sender), "NOT_ROLE_ADMIN");
+    _;
+  }
+
+  modifier onlyMinter() {
+    require(hasRole(MINTER_ROLE, msg.sender), "NOT_MINTER");
+    _;
+  }
+
+  modifier onlyStatusUpdater() {
+    require(hasRole(STATUS_UPDATER_ROLE, msg.sender), "NOT_STATUS_UPDATER");
+    _;
+  }
+
+  modifier onlyTokenURIUpdater() {
+    require(hasRole(TOKEN_URI_UPDATER_ROLE, msg.sender), "NOT_TOKEN_URI_UPDATER");
+    _;
+  }
+
+  modifier onlyStatusAndTokenURIUpdater() {
+    require(hasRole(STATUS_UPDATER_ROLE, msg.sender), "NOT_STATUS_UPDATER");
+    require(hasRole(TOKEN_URI_UPDATER_ROLE, msg.sender), "NOT_TOKEN_URI_UPDATER");
+    _;
+  }
+
+  function supportsInterface(bytes4 interfaceId) public view virtual override(ERC721, AccessControl) returns (bool) {
+        return super.supportsInterface(interfaceId);
+    }
 
   function mint(
     address _to,
     string memory _tokenURI
-  ) public onlyOwner {
+  ) public onlyMinter {
     _tokenIdCounter.increment();
     _mint(_to, _tokenIdCounter.current());
     tokens[_tokenIdCounter.current()] = DeedHashedTypes.Token(
@@ -44,7 +82,7 @@ contract DeedHashedV2 is ERC721, Ownable {
   function updateTokenStatus(
     uint256 _tokenId,
     DeedHashedTypes.TokenStatus _status
-  ) public onlyOwner {
+  ) public onlyStatusUpdater {
     require(_exists(_tokenId), "INVALID_TOKEN_ID");
     tokens[_tokenId].status = _status;
     emit TokenStatusUpdated(_tokenId, _status, tokens[_tokenId].tokenURI);
@@ -53,7 +91,7 @@ contract DeedHashedV2 is ERC721, Ownable {
   function updateTokenURI(
     uint256 _tokenId,
     string memory _tokenURI
-  ) public onlyOwner {
+  ) public onlyTokenURIUpdater {
     require(_exists(_tokenId), "INVALID_TOKEN_ID");
     tokens[_tokenId].tokenURI = _tokenURI;
     emit TokenURIUpdated(_tokenId, tokens[_tokenId].status, _tokenURI);
@@ -63,7 +101,7 @@ contract DeedHashedV2 is ERC721, Ownable {
     uint256 _tokenId,
     DeedHashedTypes.TokenStatus _status,
     string memory _tokenURI
-  ) public onlyOwner {
+  ) public onlyStatusAndTokenURIUpdater {
     require(_exists(_tokenId), "INVALID_TOKEN_ID");
     tokens[_tokenId].status = _status;
     tokens[_tokenId].tokenURI = _tokenURI;
